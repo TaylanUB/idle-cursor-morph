3b4282d15a972fb3d228ad45459ccb1a73a55fa6
Taylan Ulrich B <taylanbayirli@gmail.com>
2013-03-24 03:20:31 +0100

    Clean-up, add blinking setting, take existing non-idle settings.

--- idle-cursor-morph.el
+++ idle-cursor-morph.el
@@ -1,4 +1,33 @@
-;;; Change cursor appearance when idle.
+;;; idle-cursor-morph.el --- Morph cursor when idle.
+
+;; Copyright (C) 2013  Taylan Ulrich B.
+
+;; Author: Taylan Ulrich B. <taylanbayirli@gmail.com>
+;; Keywords: convenience
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Change the cursor-appearance after a certain time of inactivity, so it is
+;; easier to find after losing focus from the screen and later coming back.
+
+;;; Code:
+
+(defgroup idle-cursor-morph nil
+  "Morph cursor when idle."
+  :group 'convenience)
 
 (defcustom idle-cursor-timeout 20
   "Seconds to wait before morphing the cursor.
@@ -28,7 +57,7 @@ value."
                  (cons (const hbar) number))
   :group 'idle-cursor-morph)
 
-(defcustom idle-cursor-non-idle-type 'bar
+(defcustom idle-cursor-non-idle-type cursor-type
   "Type of cursor when not idle."
   :type '(choice (const t)
                  (const nil)
@@ -38,11 +67,20 @@ value."
                  (cons (const hbar) number))
   :group 'idle-cursor-morph)
 
+(defcustom idle-cursor-blinking (not no-blinking-cursor)
+  "Whether the cursor should blink when idle."
+  :type 'boolean
+  :group 'idle-cursor-morph)
+
+(defcustom idle-cursor-non-idle-blinking (not no-blinking-cursor)
+  "Whether the cursor should blink when not idle.")
+
 (defun idle-cursor-morph ()
   "Set the cursor color and type to `idle-cursor-color' and
 `idle-cursor-type', respectively."
   (set-cursor-color idle-cursor-color)
   (setq-default cursor-type idle-cursor-type)
+  (setq no-blinking-cursor (not idle-cursor-blinking))
   (add-hook 'post-command-hook 'idle-cursor-restore))
 
 (defun idle-cursor-restore ()
@@ -50,6 +88,7 @@ value."
 before `idle-cursor-morph' changed them."
   (set-cursor-color idle-cursor-non-idle-color)
   (setq-default cursor-type idle-cursor-non-idle-type)
+  (setq no-blinking-cursor (not idle-cursor-non-idle-blinking))
   (remove-hook 'post-command-hook 'idle-cursor-restore))
 
 (defvar idle-cursor-morph-timer nil)
@@ -71,3 +110,4 @@ before `idle-cursor-morph' changed them."
 (idle-cursor-activate)
 
 (provide 'idle-cursor-morph)
+;;; idle-cursor-morph.el ends here
